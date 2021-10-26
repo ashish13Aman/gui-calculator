afdf14702d851ae3358f97328de22e841231ccad
james-anmol <59534817+james-anmol@users.noreply.github.com>
2021-10-26 20:15:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from PIL import ImageTk,Image
 s_root=Tk()
 s_root.title("calculator")
 s_root.geometry("300x200")
-my_img=ImageTk.PhotoImage(Image.open("download.png"))
+my_img=ImageTk.PhotoImage(Image.open("splash.jpg"))
 my_label=Label(image=my_img)
 my_label.pack()
 
